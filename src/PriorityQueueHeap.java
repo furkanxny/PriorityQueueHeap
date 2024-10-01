cee31ce12ecba8506a49457aa90b599c823f97bc
furkan uzun <uzunf@farmingdale.edu>
2024-10-01 11:23:32 -0400

    add updated

--- src/PriorityQueueHeap.java
+++ src/PriorityQueueHeap.java
@@ -2,13 +2,14 @@ public class PriorityQueueHeap implements PriorityQueue {
     private House[] houseArray;
     private int length;
 
-
+    //Default constructor
     public PriorityQueueHeap() {
         houseArray = new House[1000];
         length = 0;
     }
 
-    PriorityQueueHeap(PriorityQueueHeap other) {
+    //Copy constructor (Deep Copy)
+   public PriorityQueueHeap(PriorityQueueHeap other) {
         houseArray = new House[other.getHouseArray().length];
 
         int count = 0;
@@ -23,25 +24,26 @@ public class PriorityQueueHeap implements PriorityQueue {
     @Override
     public void add(House a) {
         House tempHouse = new House(a);
-        int parent = (int) Math.floor(length - 1) / 2;
-        int leftChild = (int) Math.floor(2 * (parent) + 1);
-        int rightChild = (int) Math.floor(2 * (parent) + 2);
+//        int parent = (int) Math.floor(length - 1) / 2;
+//        int leftChild = (int) Math.floor(2 * (parent) + 1);
+//        int rightChild = (int) Math.floor(2 * (parent) + 2);
 
 
-        if (length == 0) {
-            houseArray[length] = tempHouse;
-        } else if (houseArray[leftChild] == null) {
-            houseArray[leftChild] = tempHouse;
-        } else if (houseArray[leftChild] != null && houseArray[rightChild] == null) {
-            houseArray[rightChild] = tempHouse;
-        }
+        houseArray[length] = tempHouse;
+//        if (length == 0) {
+//            houseArray[length] = tempHouse;
+//        } else if (houseArray[leftChild] == null) {
+//            houseArray[leftChild] = tempHouse;
+//        } else if (houseArray[leftChild] != null && houseArray[rightChild] == null) {
+//            houseArray[rightChild] = tempHouse;
+//        }
         heapfiyUp(length);
         length++;
     }
 
     public void heapfiyUp(int length) {
         int current = length;
-        int currentParent = (int) Math.floor((current - 1) / 2);
+        int currentParent = (int) (double) ((current - 1) / 2);
         while (current > 0 && houseArray[currentParent].getValue() > houseArray[current].getValue()) {
             House temp = new House(houseArray[currentParent]);
             houseArray[currentParent] = houseArray[current];
@@ -54,7 +56,7 @@ public class PriorityQueueHeap implements PriorityQueue {
     @Override
     public House getMostExpensive() {
         return null;
-    }
+    } //should remove the maximum value house from the heap and return it from the method.
 
     @Override
     public void clear() {
